2c1ac2e85cabb08382dd3a4fea88cde31cb443e4
Krzysztof Woźniak <wozniak.krzysztof124@gmail.com>
2022-02-20 13:32:08 +0100

    Remove unusual imports

--- lib/features/articles/cubit/articles_cubit.dart
+++ lib/features/articles/cubit/articles_cubit.dart
@@ -1,5 +1,4 @@
 import 'package:bloc/bloc.dart';
-import 'package:injectable/injectable.dart';
 
 import 'package:user_articles/app/core/enums.dart';
 import 'package:user_articles/domain/models/article_model.dart';
